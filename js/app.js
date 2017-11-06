8eae564c23fd5c09ca8b23e77f16addd5a775642
Chris Underwood <cunderwoodmn@gmail.com>
2017-11-05 20:18:42 -0800

    add body-Parser and begin trying to get data to flow to the view

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const app = express();
-const Twit = require('twit');
 
+const Twit = require('twit');
+const bodyParser = require('body-parser');
+const app = express();
 const config = require('./../config');
 
 const T = new Twit(config);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
 
-app.get('/', function (request, response) {
-  response.render('index');
-});
+let profileInfo = {
+  bannerImg: ""
+};
 
 T.get('account/verify_credentials', function (err, data, response) {
     // `result` is an Object with keys "data" and "resp".
@@ -18,15 +21,23 @@ T.get('account/verify_credentials', function (err, data, response) {
     // See https://github.com/ttezel/twit#tgetpath-params-callback
     // for details.
 
-    console.log('data', data);
-    console.log("img banner ", data.profile_banner_url);
+    //console.log('data', data);
+    //console.log("img banner ", data.profile_banner_url);
+    profileInfo.bannerImg = data.profile_banner_url;
     //response.send('<img src=' + data.profile_banner_url + 'alt="banner">');
-    //response.send("meow!");
+    //response.send("meow");
+    //response.locals.banner = data.profile_banner_url;
+    //console.log(profileInfo.bannerImg);
   });
 
-T.get('followers/ids',  function (err, data, response) {
-  console.log('followers/ids', data);
+app.get('/', function (request, response) {
+  console.log("profileInfo ", profileInfo);
+  response.render('index', profileInfo);
 });
+
+// T.get('followers/ids',  function (err, data, response) {
+//   console.log('followers/ids', data);
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
