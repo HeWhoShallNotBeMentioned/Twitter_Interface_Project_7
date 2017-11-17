11e71ff65429df3c42a4620a6d7876a10f8104d0
Chris Underwood <cunderwoodmn@gmail.com>
2017-11-16 21:02:20 -0800

    change how data is gathered and shared for following users and basic authentication

--- js/app.js
+++ js/app.js
@@ -6,55 +6,62 @@ const app = express();
 const config = require('./../config');
 
 const T = new Twit(config);
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
-let profileInfo = {
-  bannerImg: "",
-  profileImg: "",
-  screenName: "",
-  name: "",
-  friends: [
-    { name: "",
-      screen_name: "",
-      profile_image_url: "",
-      profile_background_image_url: ""
-    }
-  ],
-  friendsCount: ""
+// let profileInfo = {
+//   bannerImg: "",
+//   profileImg: "",
+//   screenName: "",
+//   name: "",
+//   friends: [
+//     { name: "",
+//       screen_name: "",
+//       profile_image_url: "",
+//       profile_background_image_url: ""
+//     }
+//   ],
+//   friendsCount: ""
+// };
+let credentials = (req, res, next) => {
+  T.get('account/verify_credentials', function (err, data, response) {
+      // `result` is an Object with keys "data" and "resp".
+      // `data` and `resp` are the same objects as the ones passed
+      // to the callback.
+      // See https://github.com/ttezel/twit#tgetpath-params-callback
+      // for details.
+      console.log("data.profile_banner_url ",data.profile_banner_url);
+      req.bannerImg = data.profile_banner_url;
+      req.profileImg = data.profile_image_url;
+      req.screenName = '@' + data.screen_name;
+      req.name = data.name;
+      req.friendsCount = data.friends_count;
+      //console.log("profileInfo  ", data);
+    });
+    setTimeout(next, 1000);
 };
-
-T.get('account/verify_credentials', function (err, data, response) {
-    // `result` is an Object with keys "data" and "resp".
-    // `data` and `resp` are the same objects as the ones passed
-    // to the callback.
-    // See https://github.com/ttezel/twit#tgetpath-params-callback
-    // for details.
-
-    profileInfo.bannerImg = data.profile_banner_url;
-    profileInfo.profileImg = data.profile_image_url;
-    profileInfo.screenName = '@' + data.screen_name;
-    profileInfo.name = data.name;
-    profileInfo.friendsCount = data.friends_count;
-    //console.log("profileInfo  ", data);
-  });
-
+let friends = (req, res, next) => {
   T.get('friends/list', { count: 5 },  function (err, data, response) {
     console.log('friends/list', data);
-    for (let a = 0; a < 5; a++) {
-      profileInfo.friends.name[a] = data.users[a].name;
-      profileInfo.friends.screen_name[a] = data.users[a].screen_name;
-      profileInfo.friends.profile_image_url[a] = data.users[a].profile_image_url;
-      profileInfo.friends.profile_background_image_url[a] = data.users[a].profile_background_image_url;
-    }
+      req.friends = data.users;
   });
+  setTimeout(next, 1000);
+};
+  app.use(credentials, friends);
 
-app.get('/', function (request, response) {
-  console.log("profileInfo ",  profileInfo);
-  response.render('index', {"profileInfo": profileInfo});
+app.get('/', function (req, res) {
+  //console.log("profileInfo ",  profileInfo);
+  req.params.bannerImg = req.bannerImg;
+  req.params.profileImg = req.profileImg;
+  req.params.screenName = req.screenName;
+  req.params.name = req.name;
+  req.params.friendsCount = req.friendsCount;
+  req.params.friends = req.friends;
+  console.log(req.params);
+  res.render('index', req.params);
 });
 
 
