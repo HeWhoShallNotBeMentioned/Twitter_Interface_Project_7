712a874fa867c6764da70d0717c7be7e69e41e68
Chris Underwood <cunderwoodmn@gmail.com>
2017-11-18 00:28:53 -0800

    completed code for the direct message part. left in the if else statement for determining the css for user or other message

--- js/app.js
+++ js/app.js
@@ -12,47 +12,58 @@ app.use(bodyParser.json());
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
-let credentials = (req, res, next) => {
-  T.get('account/verify_credentials', {skip_status: false}, function (err, data, response) {
-      req.bannerImg = data.profile_banner_url;
-      req.profileImg = data.profile_image_url;
-      req.screenName = '@' + data.screen_name;
-      req.screenName2 = data.screen_name;
-      req.userId = data.id_str;
-      req.name = data.name;
-      req.friendsCount = data.friends_count;
-    //console.log("profileInfo  ", data);
+  let credentials = (req, res, next) => {
+    T.get('account/verify_credentials', {skip_status: false}, function (err, data, response) {
+        req.bannerImg = data.profile_banner_url;
+        req.profileImg = data.profile_image_url;
+        req.screenName = '@' + data.screen_name;
+        req.screenName2 = data.screen_name;
+        req.userId = data.id_str;
+        req.name = data.name;
+        req.friendsCount = data.friends_count;
+      //console.log("profileInfo  ", data);
+      });
+      setTimeout(next, 1000);
+  };
+
+  let timeline = (req, res, next) => {
+    T.get('statuses/user_timeline', {screen_name: "cunderwoodmn", count: 5}, function(err, data, response) {
+      req.tweets = data;
+      //console.log("id ", req.screenName2);
+      //console.log("req.tweets ",data);
     });
     setTimeout(next, 1000);
-};
+  };
 
-let timeline = (req, res, next) => {
-  T.get('statuses/user_timeline', {screen_name: "cunderwoodmn", count: 5}, function(err, data, response) {
-    req.tweets = data;
-    //console.log("id ", req.screenName2);
-    console.log("req.tweets ",data);
-  });
-  setTimeout(next, 1000);
-};
+  let friends = (req, res, next) => {
+    T.get('friends/list', { count: 5 },  function (err, data, response) {
+      //console.log('friends/list', data);
+        req.friends = data.users;
+    });
+    setTimeout(next, 1000);
+  };
 
-let friends = (req, res, next) => {
-  T.get('friends/list', { count: 5 },  function (err, data, response) {
-    //console.log('friends/list', data);
-      req.friends = data.users;
-  });
-  setTimeout(next, 1000);
-};
-  app.use(credentials, timeline, friends);
+  let messages = (req, res, next) => {
+      T.get('direct_messages', { count: 5 },  function (err, data, response) {
+        console.log('direct_messages  ', data);
+        req.messages = data;
+      });
+      setTimeout(next, 1000);
+  };
+
+  app.use(credentials, timeline, friends, messages);
 
 app.get('/', function (req, res) {
   //console.log("profileInfo ",  profileInfo);
   req.params.bannerImg = req.bannerImg;
   req.params.profileImg = req.profileImg;
   req.params.screenName = req.screenName;
+  req.params.screenName2 = req.screenName2;
   req.params.name = req.name;
   req.params.friendsCount = req.friendsCount;
   req.params.friends = req.friends;
   req.params.tweets = req.tweets;
+  req.params.messages = req.messages;
   //console.log(req.params);
   res.render('index', req.params);
 });
